ee28e4df81aefe791668b1ad500e3f591b96c5db
jessamynsmith <geekchick77@gmail.com>
2022-06-28 18:15:17 -0600

    Add STATIC_ROOT to settings

--- pdfjs2image_example/settings.py
+++ pdfjs2image_example/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = MEDIA_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'pdfjs2image_example' / 'static'
 ]
